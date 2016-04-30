2ae58901711e92fd54e669416268212861eae33b
Sultanb <esultan.bouhamdan@gmail.com>
2016-04-30 15:39:15 +0100

    Update cloud HA.c

--- cloud HA.c
+++ cloud HA.c
@@ -38,34 +38,3 @@ int countNs(list_t * xs, intptr_t n) {
   return length(filter(isN, (void*)n, xs));
 }
 
-/*
-  The promised explanation: Some of the client code above tries to use
-  a number for the environment by using a number (intptr_t) where a
-  pointer (void *) is expected.  This is technically not allowed: any
-  pointer (including void*) can be cast to intptr_t (always) and the
-  result can be cast back to the pointer type, but that is different
-  than starting with an intptr_t and casting it to void* and then back
-  to intptr_t.
-
-  It appears there is no legal, portable way to create a number that
-  can be cast to void* and back.  People do this sort of thing often,
-  but the resulting code is not strictly portable.  So what should we
-  do for our closures example besides ignore this and write
-  non-portable code using int or intptr_t?
-  
-  Option 1 is to use an int* for the environment, passing a pointer to the
-  value we need.  That is the most common approach and what we need to do for
-  environments with more than one value in them anyway.  For the examples above,
-  it would work to pass the address of a stack-allocated int, but that works
-  only because the higher-order functions we are calling will not store those
-  pointers in places where they might be used later after the stack variable
-  is deallocated.  So it works fine for examples like map and filter, but would
-  not work for callback idioms.  For those, the pointer should refer to memory
-  returned by malloc or a similar library function.
-
-  Option 2 is to change our approach to have the higher-order functions use
-  intptr_t for the type of the environment instead of void*.  This works in
-  general since other code can portably cast from any pointer type to
-  intptr_t.  It is a less standard approach -- one commonly sees void* used
-  as we have in the code above.
-*/
